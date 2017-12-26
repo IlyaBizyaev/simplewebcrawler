10ae72023885a56aaa3f25b45dd949f5f2187766
Ilya Bizyaev <bizyaev@zoho.com>
2017-12-26 14:27:37 +0300

    General code structure

--- SimpleWebCrawler.java
+++ SimpleWebCrawler.java
@@ -46,114 +46,168 @@ public class SimpleWebCrawler implements WebCrawler {
     }
 
     private static String generateLocalFilename(String url) {
-        return url.replaceAll("://", "_").replaceAll("/", "_");
+        try {
+            final String encoded = URLEncoder.encode(url, "UTF-8");
+            return encoded.length() < 200 ? encoded : encoded.substring(0, 200);
+        } catch (UnsupportedEncodingException e) {
+            return url.replaceAll("://", "_").replaceAll("/", "_");
+        }
+    }
+
+    private static String removeAnchor(String url) {
+        for (int i = 0; i < url.length(); i++) {
+            if (url.charAt(i) == '#') {
+                return url.substring(0, i);
+            }
+        }
+        return url;
     }
 
     @Override
     public Page crawl(String url, int depth) {
-        URL currentPage;
-        try {
-            currentPage = new URL(url);
-        } catch (MalformedURLException e) {
-            System.out.println(url + " is not a valid URL");
-            return null;
-        }
-
-        String pageContents = null;
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(downloader.download(url), "utf8"))) {
-            StringBuilder text = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                text.append(inputLine);
-            pageContents = text.toString();
-        } catch (UnsupportedEncodingException e) {
-            System.out.println("UTF-8 is not supported on this machine");
-        } catch (IOException e) {
-            System.out.println("Page read failed: " + e.getMessage());
-        }
-
-        if (pageContents == null) {
-            return new Page(url, "");
-        }
-
-        pageContents = deleteComments(pageContents);
-
-        String title = null;
-        for (String tag : extractTag(pageContents, "title")) {
-            title = tagInsides(tag);
-            if (title != null) {
-                break;
+        class Task {
+            private Task(String url, int depth) {
+                this.url = url;
+                this.depth = depth;
             }
+            private String url;
+            private int depth;
         }
-        Page result = new Page(url, title);
+        Deque<Task> tasks = new ArrayDeque<>();
+        tasks.addLast(new Task(url, depth));
 
-        if (depth == 0) {
-            return result;
-        }
+        Map<String, Page> processed = new HashMap<>();
+        List<String> visitHistory = new ArrayList<>();
+        Map<String, List<String>> pageChildren = new HashMap<>();
 
-        List<String> imgList = extractTag(pageContents, "img");
-        Set<String> currentPageImages = new HashSet<>();
-        Set<String> currentPageChildren = new HashSet<>();
-        for (String tag : imgList) {
-            String imgLink = extractAttribute(tag, "src");
-            if (imgLink == null) {
+        while (!tasks.isEmpty()) {
+            Task currentTask = tasks.pollFirst();
+
+            if (processed.containsKey(currentTask.url)) {
                 continue;
             }
-            URL imgUrl;
+
+            visitHistory.add(currentTask.url);
+
+            if (currentTask.depth == 0) {
+                processed.put(currentTask.url, new Page(currentTask.url, ""));
+                continue;
+            }
+
+            URL currentPageUrl;
             try {
-                imgUrl = new URL(currentPage, imgLink);
+                currentPageUrl = new URL(currentTask.url);
             } catch (MalformedURLException e) {
-                System.out.println(imgLink + " is not a valid URL");
+                System.out.println(currentTask.url + " is not a valid URL");
                 continue;
             }
-            String completeImgUrl = imgUrl.toString();
-            if (!currentPageImages.contains(completeImgUrl)) {
-                System.out.println(generateLocalFilename(completeImgUrl));
-                try {
-                    ReadableByteChannel rbc = Channels.newChannel(downloader.download(completeImgUrl));
-                    FileOutputStream fos = new FileOutputStream(generateLocalFilename(completeImgUrl));
-                    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                    Image img = new Image(completeImgUrl, generateLocalFilename(completeImgUrl));
-                    result.addImage(img);
-                    currentPageImages.add(completeImgUrl);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Could not create " + generateLocalFilename(completeImgUrl));
-                } catch (IOException e) {
-                    System.out.println("Could not download " + completeImgUrl);
-                }
-            }
-        }
 
-        List<String> aList = extractTag(pageContents, "a");
-        for (String tag : aList) {
-            String childLink = extractAttribute(tag, "href");
-            if (childLink == null) {
-                continue;
+            String pageContents = null;
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(downloader.download(currentTask.url), "utf8"))) {
+                StringBuilder text = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    text.append(inputLine);
+                pageContents = text.toString();
+            } catch (UnsupportedEncodingException e) {
+                System.out.println("UTF-8 is not supported on this machine");
+            } catch (IOException e) {
+                System.out.println("Page read failed: " + e.getMessage());
             }
-            URL childUrl;
-            try {
-                childUrl = new URL(currentPage, childLink);
-            } catch (MalformedURLException e) {
-                System.out.println(childLink + " is not a valid URL");
-                continue;
+
+            if (pageContents == null) {
+                return new Page(currentTask.url, "");
             }
-            String completeChildUrl = childUrl.toString();
-            for (int i = 0; i < completeChildUrl.length(); i++) {
-                if (completeChildUrl.charAt(i) == '#') {
-                    completeChildUrl = completeChildUrl.substring(0, i);
+
+            String title = null;
+            for (String tag : extractTag(pageContents, "title")) {
+                title = tagInsides(tag);
+                if (title != null) {
                     break;
                 }
             }
-            if (!currentPageChildren.contains(completeChildUrl)) {
-                Page childPage = crawl(completeChildUrl, depth - 1);
-                if (childPage != null) {
-                    result.addLink(childPage);
+            Page currentPage = new Page(currentTask.url, title);
+
+            pageContents = deleteComments(pageContents);
+
+            List<String> imgList = extractTag(pageContents, "img");
+            Set<String> currentPageImages = new HashSet<>();
+            for (String tag : imgList) {
+                String imgLink = extractAttribute(tag, "src");
+                if (imgLink == null) {
+                    continue;
                 }
-                currentPageChildren.add(completeChildUrl);
+                URL imgUrl;
+                try {
+                    imgUrl = new URL(currentPageUrl, imgLink);
+                } catch (MalformedURLException e) {
+                    System.out.println(imgLink + " is not a valid URL");
+                    continue;
+                }
+                String completeImgUrl = imgUrl.toString();
+                if (!currentPageImages.contains(completeImgUrl)) {
+                    String localFilename = generateLocalFilename(completeImgUrl);
+                    try {
+                        ReadableByteChannel rbc = Channels.newChannel(downloader.download(completeImgUrl));
+                        FileOutputStream fos = new FileOutputStream(localFilename);
+                        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                        Image img = new Image(completeImgUrl, localFilename);
+                        currentPage.addImage(img);
+                        currentPageImages.add(completeImgUrl);
+                    } catch (FileNotFoundException e) {
+                        System.out.println("Could not create " + localFilename);
+                    } catch (IOException e) {
+                        System.out.println("Could not download " + completeImgUrl);
+                    }
+                }
+            }
+
+            Set<String> currentPageChildren = new HashSet<>();
+            List<String> aList = extractTag(pageContents, "a");
+            for (String tag : aList) {
+                String childLink = extractAttribute(tag, "href");
+                if (childLink == null) {
+                    continue;
+                }
+                URL childUrl;
+                try {
+                    childUrl = new URL(currentPageUrl, childLink);
+                } catch (MalformedURLException e) {
+                    System.out.println(childLink + " is not a valid URL");
+                    continue;
+                }
+                String completeChildUrl = childUrl.toString();
+
+                if (!currentPageChildren.contains(completeChildUrl)) {
+                    tasks.addLast(new Task(completeChildUrl, currentTask.depth - 1));
+                    pageChildren.putIfAbsent(currentTask.url, new ArrayList<>());
+                    pageChildren.get(currentTask.url).add(removeAnchor(completeChildUrl));
+                    currentPageChildren.add(completeChildUrl);
+                }
+            }
+
+            processed.put(currentTask.url, currentPage);
+        }
+
+        /*for (Map.Entry<String, List<String>> entry : pageChildren.entrySet()) {
+            String parent = entry.getKey();
+            for (String child : entry.getValue()) {
+                processed.get(parent).addLink(processed.get(child));
+            }
+        }*/
+
+        ListIterator li = visitHistory.listIterator(visitHistory.size());
+
+        while (li.hasPrevious()) {
+            String parent = (String) li.previous();
+            if (pageChildren.containsKey(parent)) {
+                for (String child : pageChildren.get(parent)) {
+                    processed.get(parent).addLink(processed.get(child));
+                }
             }
         }
 
-        return result;
+        return processed.get(url);
     }
 }
